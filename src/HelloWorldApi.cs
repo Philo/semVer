9551a81ef822f4b78f6cd6d365ab74ba6bacd28d
philo <phil@stormid.com>
2015-08-15 16:06:00 +0100

    Adds sample project

---
+++ src/HelloWorldApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class HelloWorldApi
+    {
+        public string Greet(string name)
+        {
+            return $@"Hello {name}";
+        }
+
+        public string Leave(string name)
+        {
+            return $@"Goodbye {name}";
+        }
+    }
+}
